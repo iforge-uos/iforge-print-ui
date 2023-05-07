5166b1f08203cf7b6a96dc1086897806ac71bb4c
Sam Piper <sampii@protonmail.com>
2023-05-07 18:25:03 +0100

    feat: added population of user_data in the user-nav.tsx file as well as some other cleanups

--- components/auth/user-nav.tsx
+++ components/auth/user-nav.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link"
 import { LogOut, User } from "lucide-react"
+import { useCookies } from "react-cookie"
 
 import { useAuth } from "@/lib/auth-context"
+import { getGravatarUrl } from "@/lib/utils"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import {
@@ -16,26 +18,36 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function UserNav() {
+  const [cookies] = useCookies(["user"]) // Read the "user" cookie
+  const user = cookies.user
+
   const { logout } = useAuth()
   const handleLogout = () => {
     logout()
   }
+
+  const gravatarUrl = getGravatarUrl(user.email) // Generate Gravatar URL
+  const userInitials = user.name
+    .split(" ")
+    .map((namePart: string) => namePart.charAt(0).toUpperCase())
+    .join("")
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage src={gravatarUrl} alt={userInitials} />
+            <AvatarFallback>{userInitials}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">{user.uid}</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {user.email}
             </p>
           </div>
         </DropdownMenuLabel>
--- lib/auth-context.tsx
+++ lib/auth-context.tsx
@@ -67,7 +67,6 @@ export function AuthProvider({ children }: AuthProviderProps) {
         }
       )
 
-      console.log("Success:", response.data)
       const { access_token, refresh_token, user } = response.data.data
       setCookie("access_token", access_token)
       setCookie("refresh_token", refresh_token)
