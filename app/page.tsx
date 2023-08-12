95e1ca301e195e4b8c511a99417fe5da50c1c63e
charliepiper <charliepiperr@gmail.com>
2023-08-12 18:51:49 +0100

    fix: width issue on small screens

--- app/page.tsx
+++ app/page.tsx
@@ -63,26 +63,25 @@ export default function IndexPage() {
             {/* https://undraw.co/search */}
             {/* <Image src="" className="w-500 h-auto" alt="homepage image" placeholder="blur"></Image> */}
         </div>
-        <div className="w-2/3 pl-20">
+        <div className=" pl-20">
           <div className = "max-w-xl">
-          <p className="text-sm font-extrabold text-muted-foreground">PRINTING</p>
-          <h1 className= "scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">dolor sit amet, consectetur adipiscing elit</h1>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sodales porttitor purus, nec elementum neque malesuada in. Suspendisse potenti. Proin non dolor eu leo fermentum elementum ac nec sem. Suspendisse vehicula sit amet urna ut cursus.
-          consectetur adipiscing elit.
-          </p>
-          <div className = "pt-5">
-            <Link
-            href="/submit"
-            rel="noreferrer"
-            className={buttonVariants({ size: "lg" })}
-            >
-            get started
-          </Link>
-          </div>
-
-        </div>
-          </div>
+            <p className="text-sm font-extrabold text-muted-foreground">PRINTING</p>
+              <h1 className= "scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">dolor sit amet, consectetur adipiscing elit</h1>
+                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sodales porttitor purus, nec elementum neque malesuada in. Suspendisse potenti. Proin non dolor eu leo fermentum elementum ac nec sem. Suspendisse vehicula sit amet urna ut cursus.
+                  consectetur adipiscing elit.
+                </p>
+                <div className = "pt-5">
+                  <Link
+                    href="/submit"
+                    rel="noreferrer"
+                    className={buttonVariants({ size: "lg" })}
+                    >
+                    get started
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
     </section>
   )
