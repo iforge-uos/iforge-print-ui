100669448ef943fc5cf9c054bd7af1e70e7f4e68
charliepiper <charliepiperr@gmail.com>
2023-08-31 14:42:21 +0100

    feat: added styling to user settings form

---
+++ app/users/[slug]/layout.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { SidebarNav } from "@/app/users/components/sidebar-nav"
+import { Separator }from "@/components/ui/separator"
+import Link from 'next/link';
+
+import { useParams } from "next/navigation";
+
+
+// import { useRouter } from 'next/router'
+// const router = useRouter()
+
+
+import { usePathname } from "next/navigation";
+
+
+  
+  interface SettingsLayoutProps {
+    children: React.ReactNode
+  }
+  
+  export default function SettingsLayout({ children }: SettingsLayoutProps) {
+    const pathname = usePathname();
+    const slug = (pathname.split('/').pop())
+
+    // const router = useRouter();
+
+    const sidebarNavItems = [
+      {
+        title: "Settings",
+        href: `/users/${slug}/`,      
+      },
+
+  
+    ]
+    
+    return (
+      <>
+        <div className="hidden space-y-6 p-10 pb-16 md:block">
+          <div className="space-y-0.5">
+            <h2 className="text-2xl font-bold tracking-tight">User information</h2>
+            <p className="text-muted-foreground">
+              View user data and update their settings.
+            </p>
+          </div>
+          <Separator className="my-6" />
+          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+            <aside className="-mx-4 lg:w-1/5">
+              <SidebarNav items={sidebarNavItems} />
+            </aside>
+            <div className="flex-1 lg:max-w-2xl">{children}</div>
+          </div>
+        </div>
+      </>
+    )
+}
--- app/users/[slug]/page.tsx
+++ app/users/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import UserForm from "@/app/users/[slug]/user-form"
+import { Separator }from "@/components/ui/separator"
+
 
 function isInteger(str: string): boolean {
   return /^\d+$/.test(str)
@@ -11,7 +13,18 @@ export default function ManageUser({ params }: { params: { slug: string } }) {
 
   return (
     <div className="space-y-6">
-      <UserForm slug={parseInt(params.slug, 10)} />
+          <div className="container">
+            <div className="">
+              <div className="text-2xl font-semibold">User settings</div>
+               <p className="text-sm text-muted-foreground">
+                This is where you can change a user's settings.
+              </p>
+            </div>
+          <Separator />
+            <div className="py-5">
+              < UserForm slug={parseInt(params.slug, 10)} />
+            </div>
+          </div>
     </div>
   )
 }
